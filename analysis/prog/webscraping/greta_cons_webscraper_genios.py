f86248dcd8b37ffb54da8cc0edf41bdc26052df9
Unknown <fabelm@g.cofc.edu>
2020-01-16 18:43:14 +0100

    add scraping for FFF  (special encoding for |)

--- analysis/prog/webscraping/greta_cons_webscraper_genios.py
+++ analysis/prog/webscraping/greta_cons_webscraper_genios.py
@@ -28,9 +28,10 @@ z_start_date = datetime(2018, 1, 1)    # 2018, 1, 1
 z_end_date   = datetime(2019, 12, 31)  # 2019, 12, 31
 
 # HOME directories
-z_media_output =     '/Users/marcfabel/Dropbox/greta_consumption_Dx/analysis/data/intermediate/media/'
+#z_media_output =     '/Users/marcfabel/Dropbox/greta_cons_Dx/analysis/data/intermediate/media/'
 
-
+# work directories (LOCAL)
+z_media_output =     'C:/Users/fabel/Dropbox/greta_cons_Dx/analysis/data/intermediate/media/'
 
 ###############################################################################
 #       1) Define programs
@@ -47,7 +48,7 @@ def daterange(start_date, end_date):
 def scraper(term, output_name):
     fh_write = open(z_media_output + output_name, 'w')
     fh_write.write('The following terms were used: ' + ',' + term + '\n')
-    
+
     # loop through days
     for day in daterange(z_start_date, z_end_date):
         link = 'https://www.genios.de/dosearch?explicitSearch=true&q='+term+'&\
@@ -57,18 +58,18 @@ CKV%2CZ2=&CO%2CC2%2CTA%2CKA%2CVA%2CZ1=&CT%2CZ4%2CKW=&BR%2CGW%2CN1%2CN2\
 %2CNC%2CND%2CSC%2CWZ%2CZ5%2CAI%2CBC%2CKN%2CTN%2CVN%2CK0%2CB4%2CNW%2CVH\
 =&Z3%2CCN%2CCE%2CKC%2CTC%2CVC=&timeFilterType=on&DT_from=\
 '+ day.strftime('%d.%m.%Y') + '&DT_to=' + day.strftime('%d.%m.%Y') + '&x=61&y=18'
-        
+
         # extract html
         page = requests.get(link)
         soup = BeautifulSoup(page.content, 'html.parser')
         result_text_header = soup.find(class_="moduleResultTextHeader")
         result = result_text_header.get_text()
-            
+
         #omit the thousands digit
-        if len(result)<= 22: 
+        if len(result)<= 22:
             print(day.strftime('%d.%m.%Y'), result[18:-1])
             fh_write.write(day.strftime('%d.%m.%Y') + ',' + result[18:-1] + '\n')
-        if len(result)== 24: 
+        if len(result)== 24:
             print(day.strftime('%d.%m.%Y'), result[18:19]+result[20:-1])
             fh_write.write(day.strftime('%d.%m.%Y') + ',' + result[18:19]+result[20:-1] + '\n')
         if len(result)== 25:
@@ -81,18 +82,29 @@ CKV%2CZ2=&CO%2CC2%2CTA%2CKA%2CVA%2CZ1=&CT%2CZ4%2CKW=&BR%2CGW%2CN1%2CN2\
     # close file handle
     fh_write.close()
     print('finished scraping for terms: ' + term)
-    
+
 
 
 
 ###############################################################################
 #       2) scrape for terms
-###############################################################################  
+###############################################################################
 
+
+# Thunberg
+term_thunberg = 'thunberg'
+scraper(term_thunberg, 'genios_articles_thunberg.csv')
 
 # Greta Thunberg
-term_greta = 'thunberg'
-scraper(term_greta, 'genios_articles_greta.csv')
+term_greta_thunberg = "\"greta thunberg\""
+scraper(term_greta_thunberg, 'genios_articles_greta_thunberg.csv')
+
+# Fridays For Future
+term_FFF = "\"Fridays-For-Future\"+%7C%7C+Klimastreik+%7C%7C+\"FridaysForFuture\""
+scraper(term_FFF, 'genios_articles_FFF.csv')
+# ASCII Encoding Reference: |      %7C
+# Boolean Operator in search engine -> || used for OR
+# source for encoding: https://www.w3schools.com/tags/ref_urlencode.ASP
 
 # universe of articles
 term_all_paper = ''
@@ -100,4 +112,3 @@ scraper(term_all_paper, 'genios_articles_all.csv')
 
 
 
-      
