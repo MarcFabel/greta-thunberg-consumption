f67e4bdd42cf01f5cf5f9a8905ed9ff5391f8180
Unknown <fabelm@g.cofc.edu>
2020-01-21 11:32:47 +0100

    Figures about FFF

--- analysis/prog/prepare/greta_cons_prepare_articles.py
+++ analysis/prog/prepare/greta_cons_prepare_articles.py
@@ -24,16 +24,17 @@ import matplotlib.dates as mdates
 #style.available
 style.use('seaborn-darkgrid')
 import pandas as pd
-import seaborn as sns
 
 
 # HOME directories
-#z_media_input =     '/Users/marcfabel/Dropbox/greta_consumption_Dx/analysis/data/intermediate/media/'
+z_media_input =     '/Users/marcfabel/Dropbox/greta_cons_Dx/analysis/data/intermediate/media/'
+
+
 
 # work directories (LOCAL)
-z_media_input =     'C:/Users/fabel/Dropbox/greta_cons_Dx/analysis/data/intermediate/media/'
-z_media_figures =   'G:/Projekte/Projekte_ab2016/greta_cons/analysis/output/graphs/descriptive/'
-z_prefix =          'greta_cons_'
+#z_media_input =     'C:/Users/fabel/Dropbox/greta_cons_Dx/analysis/data/intermediate/media/'
+#z_media_figures =   'G:/Projekte/Projekte_ab2016/greta_cons/analysis/output/graphs/descriptive/'
+#z_prefix =          'greta_cons_'
 
 ###############################################################################
 #           Read in Data
@@ -114,44 +115,36 @@ plt.savefig(z_media_figures + z_prefix + 'greta_per_1000_2018_2019.pdf')
 
 
 
-########## plot for the year 2019 #############################################
-c_shading = 'darkgreen'
-c_opacity = 0.3
-
-fig, ax = plt.subplots()
-ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_greta, alpha=0.3, color='darkgrey')
-ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_greta_ma3)
-ax.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
-ax.xaxis.set_minor_locator(mdates.YearLocator())
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
-ax.xaxis.set_minor_formatter(mdates.DateFormatter('\n%Y'))
-#events
-ax.axvspan(datetime(2018,12,3), datetime(2018,12,14), color=c_shading, alpha=c_opacity)   # Katowice - climate conference
-ax.axvspan(datetime(2019,1,23), datetime(2019,1,25), color=c_shading, alpha=c_opacity)    # Davos - world economic forum
-ax.axvline(datetime(2019,3,15), color=c_shading, alpha=c_opacity)                         # worldwide - 1.4 million young people go on strike
-ax.axvline(datetime(2019,4,16), color=c_shading, alpha=c_opacity)                         # Strasbourg - EU speech
-ax.axvspan(datetime(2019,8,14), datetime(2019,8,28), color=c_shading, alpha=c_opacity)    # Atlantic - travel to UN
-ax.axvspan(datetime(2019,9,23), datetime(2019,9,29), color=c_shading, alpha=c_opacity)    # NY - UN climate action summit
-ax.axvspan(datetime(2019,12,2), datetime(2019,12,13), color=c_shading, alpha=c_opacity)   # Madrid - UN climate change conference
-#plt.savefig(z_media_figures + z_prefix + 'greta_events_2019.pdf')
-
-
-articles.loc['2019'].art_greta.plot()
-articles.loc['2019'].art_greta_ma3.plot()
-fig = plt.gcf()
-fig.savefig(z_media_figures + z_prefix + 'greta_2019.pdf')
-plt.show()
-
-
 ########## plot greta articles per 1,000 articles - 2019 ######################
+c_shading = 'darkgreen'
+c_opacity = 0.4
+
+
+# without events
 fig, ax = plt.subplots()
-ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_greta_ratio, alpha=0.3, color='darkgrey')
-ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_greta_ratio_ma3)
+ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_greta_ratio, alpha=0.6, color='darkgrey', label='unsmoothed')
+ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_greta_ratio_ma3, label='smoothed')
 ax.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
 ax.xaxis.set_minor_locator(mdates.YearLocator())
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
 ax.xaxis.set_minor_formatter(mdates.DateFormatter('\n%Y'))
-#events
+ax.legend()
+ax.set(xlabel='Date', ylabel='Number of articles covering Greta Thunberg,\n per 1,000 articles') #xlabel='months',
+#plt.savefig(z_media_figures + z_prefix + 'greta_per_1000_2019.pdf')
+
+
+
+
+# with events
+fig, ax = plt.subplots()
+ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_greta_ratio, alpha=0.006, color='darkgrey')
+ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_greta_ratio_ma3, label='smoothed')
+ax.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
+ax.xaxis.set_minor_locator(mdates.YearLocator())
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+ax.xaxis.set_minor_formatter(mdates.DateFormatter('\n%Y'))
+ax.set(xlabel='Date', ylabel='Number of articles covering Greta Thunberg,\n per 1,000 articles') #xlabel='months',
+
 ax.axvspan(datetime(2018,12,3), datetime(2018,12,14), color=c_shading, alpha=c_opacity)   # Katowice - climate conference
 ax.axvspan(datetime(2019,1,23), datetime(2019,1,25), color=c_shading, alpha=c_opacity)    # Davos - world economic forum
 ax.axvline(datetime(2019,3,1), color=c_shading, alpha=c_opacity)                          # Hamburg - climate strike with Greta (friday)
@@ -164,40 +157,60 @@ ax.axvspan(datetime(2019,12,2), datetime(2019,12,13), color=c_shading, alpha=c_o
 #plt.savefig(z_media_figures + z_prefix + 'greta_per_1000_events_2019.pdf')
 
 
-articles.loc['2019'].art_greta_ratio.plot()
-articles.loc['2019'].art_greta_ratio_ma3.plot()
-fig = plt.gcf()
-fig.savefig(z_media_figures + z_prefix + 'greta_per_1000_2019.pdf')
-plt.show()
 
 
 
 ########## Fridays For Future #################################################
 
-# plot fff articles for entire time horizon
-articles.art_fff.plot()
-fig = plt.gcf()
-fig.savefig(z_media_figures + z_prefix + 'fff_2018_2019.pdf')
-plt.show()
 
-#plot fff articles per 1,000 articles for entire time horizon
-articles.art_fff_ratio.plot()
-articles.art_fff_ratio_ma7.plot()
-fig = plt.gcf()
-fig.savefig(z_media_figures + z_prefix + 'fff_per_1000_2018_2019.pdf')
-plt.show()
+########## plot fff articles per 1,000 articles - 2019 ######################
 
 
-# plot for the year 2019
-articles.loc['2019'].art_fff.plot()
-fig = plt.gcf()
-fig.savefig(z_media_figures + z_prefix + 'fff_2019.pdf')
-plt.show()
+# without events
+fig, ax = plt.subplots()
+ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_fff_ratio, alpha=0.6, color='darkgrey', label='unsmoothed')
+ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_fff_ratio_ma3, label='smoothed')
+ax.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
+ax.xaxis.set_minor_locator(mdates.YearLocator())
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+ax.xaxis.set_minor_formatter(mdates.DateFormatter('\n%Y'))
+ax.legend()
+ax.set(xlabel='Date', ylabel='Number of articles covering FFF,\n per 1,000 articles') #xlabel='months',
+#plt.savefig(z_media_figures + z_prefix + 'fff_per_1000_events_2019.pdf')
 
 
-# plot fff articles per 1,000 articles
-articles.loc['2019'].art_fff_ratio.plot()
-articles.loc['2019'].art_fff_ratio_ma3.plot()
-fig = plt.gcf()
-fig.savefig(z_media_figures + z_prefix + 'fff_per_1000_2019.pdf')
-plt.show()
+
+
+#events
+fig, ax = plt.subplots()
+ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_fff_ratio, alpha=0.006, color='darkgrey', label='unsmoothed')
+ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_fff_ratio_ma3, label='smoothed')
+ax.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
+ax.xaxis.set_minor_locator(mdates.YearLocator())
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+ax.xaxis.set_minor_formatter(mdates.DateFormatter('\n%Y'))
+ax.set(xlabel='Date', ylabel='Number of articles covering FFF,\n per 1,000 articles')
+ax.axvspan(datetime(2018,12,3), datetime(2018,12,14), color=c_shading, alpha=c_opacity)   # Katowice - climate conference
+ax.axvspan(datetime(2019,1,23), datetime(2019,1,25), color=c_shading, alpha=c_opacity)    # Davos - world economic forum
+ax.axvline(datetime(2019,3,1), color=c_shading, alpha=c_opacity)                          # Hamburg - climate strike with Greta (friday)
+ax.axvline(datetime(2019,3,15), color=c_shading, alpha=c_opacity)                         # worldwide - 1.4 million young people go on strike (friday)
+ax.axvline(datetime(2019,3,29), color=c_shading, alpha=c_opacity)                         # Berlin - climate strike with Greta (friday)
+ax.axvline(datetime(2019,4,16), color=c_shading, alpha=c_opacity)                         # Strasbourg - EU speech
+ax.axvspan(datetime(2019,8,14), datetime(2019,8,28), color=c_shading, alpha=c_opacity)    # Atlantic - travel to UN
+ax.axvspan(datetime(2019,9,23), datetime(2019,9,29), color=c_shading, alpha=c_opacity)    # NY - UN climate action summit
+ax.axvspan(datetime(2019,12,2), datetime(2019,12,13), color=c_shading, alpha=c_opacity)   # Madrid - UN climate change conference
+#plt.savefig(z_media_figures + z_prefix + 'greta_per_1000_events_2019.pdf')
+
+
+
+#plot both in one graph
+fig, ax = plt.subplots()
+ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_greta_ratio_ma3)
+ax.plot(articles.loc['2018-11':].index.values, articles.loc['2018-11':].art_fff_ratio_ma3)
+ax.xaxis.set_major_locator(mdates.MonthLocator(interval=2))
+ax.xaxis.set_minor_locator(mdates.YearLocator())
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+ax.xaxis.set_minor_formatter(mdates.DateFormatter('\n%Y'))
+
+
+
