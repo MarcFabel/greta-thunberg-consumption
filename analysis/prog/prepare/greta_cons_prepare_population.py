215e0a564182bf3e27343819fc910773bd12fe55
Unknown <fabelm@g.cofc.edu>
2021-01-19 21:27:32 +0100

    add 2014 & (2019-2014) growth variables

--- analysis/prog/prepare/greta_cons_prepare_population.py
+++ analysis/prog/prepare/greta_cons_prepare_population.py
@@ -53,14 +53,14 @@ z_prefix =                      'greta_cons'
 
 
 ###############################################################################
-#           12_POPULATION
+#           124_CURRENT_POPULATION
 ###############################################################################
 
 
 
 
 
-########## 124_CURRENT_POPULATION ##########
+########## 2019 ###############################################################
 pop_raw = pd.read_excel(z_regional_source + 'population_agegroups_2019_ags8.xlsx',
                     skiprows=6, skipfooter=33 ) 
 
@@ -106,9 +106,89 @@ pop_ags5 = pop_ags5.loc[pop_ags5['pop_t'] != '-']
 pop_ags5['ags_name'] = pop_ags5['ags_name'].str.lstrip() # remove preceding space
 
 
+pop_ags5_2019 = pop_ags5.copy()
+pop_ags8_2019 = pop_ags8.copy()
+
+
+
+
+
+
+########## 2014 ###############################################################
+pop_raw = pd.read_excel(z_regional_source + 'population_agegroups_2014_ags8.xlsx',
+                    skiprows=6, skipfooter=33 ) 
+
+pop_raw.columns = ['ags', 'ags_name', 'pop_0_2_t', 'pop_3_5_t', 'pop_6_9_t', 'pop_10_14_t', 'pop_15_17_t', 'pop_18_19_t', 'pop_20_24_t',
+		'pop_25_29_t', 'pop_30_34_t', 'pop_35_39_t', 'pop_40_44_t' , 'pop_45_49_t', 'pop_50_54_t', 'pop_55_59_t',
+		'pop_60_64_t', 'pop_65_74_t', 'pop_74+_t', 'pop_t',
+
+		'pop_0_2_m', 'pop_3_5_m', 'pop_6_9_m', 'pop_10_14_m', 'pop_15_17_m', 'pop_18_19_m', 'pop_20_24_m',
+		'pop_25_29_m', 'pop_30_34_m', 'pop_35_39_m', 'pop_40_44_m' , 'pop_45_49_m', 'pop_50_54_m', 'pop_55_59_m',
+		'pop_60_64_m', 'pop_65_74_m', 'pop_74+_m', 'pop_m',
+
+		'pop_0_2_f', 'pop_3_5_f', 'pop_6_9_f', 'pop_10_14_f', 'pop_15_17_f', 'pop_18_19_f', 'pop_20_24_f',
+		'pop_25_29_f', 'pop_30_34_f', 'pop_35_39_f', 'pop_40_44_f' , 'pop_45_49_f', 'pop_50_54_f', 'pop_55_59_f',
+		'pop_60_64_f', 'pop_65_74_f', 'pop_74+_f', 'pop_f']
+
+# keep only relevant columns
+pop_raw = pop_raw[['ags', 'ags_name', 'pop_t', 'pop_0_2_t', 'pop_3_5_t', 'pop_6_9_t', 
+                   'pop_10_14_t', 'pop_15_17_t', 'pop_18_19_t', 'pop_20_24_t',
+                   'pop_25_29_t', 'pop_30_34_t', 'pop_35_39_t', 'pop_40_44_t',
+                   'pop_45_49_t', 'pop_50_54_t', 'pop_55_59_t',	'pop_60_64_t', 
+                   'pop_65_74_t', 'pop_74+_t']]
+
+
+# correct Hamburg and Berlin to be in ags8
+pop_raw['ags'] = pop_raw['ags'].replace({'02':'02000000','11':'11000000'})
+
+
+# prepare ags8 population
+pop_ags8 = pop_raw.loc[pop_raw['ags'].str.len() == 8]
+pop_ags8 = pop_ags8.loc[pop_ags8['pop_t'] != '-'] # keep only values with entries (the data set contains deprecated ags keys)
+# replace '-' with zeros
+z_pop_cols = pop_raw.columns.drop(['ags', 'ags_name', 'pop_t']).tolist()
+pop_ags8[z_pop_cols] = pop_ags8[z_pop_cols].replace('-', '0')
+pop_ags8['ags_name'] = pop_ags8['ags_name'].str.lstrip() # remove preceding space
+
+
+# correct Hamburg and Berlin to be in ags8
+pop_raw['ags'] = pop_raw['ags'].replace({'02000000':'02000','11000000':'11000'})
+
+# prepare ags5 population
+pop_ags5 = pop_raw.loc[pop_raw['ags'].str.len() == 5]
+pop_ags5 = pop_ags5.loc[pop_ags5['pop_t'] != '-'] 
+pop_ags5['ags_name'] = pop_ags5['ags_name'].str.lstrip() # remove preceding space
+
+
+pop_ags5_2014 = pop_ags5.copy()
+pop_ags8_2014 = pop_ags8.copy()
+
+
+
+
+
+########## combine the years ##################################################
+
+# correct for district reform
+pop_ags5_2014.loc[pop_ags5_2014['ags']=='03156', 'ags'] = '03159'
+pop_ags5_2014.loc[pop_ags5_2014['ags']=='03152', 'ags'] = '03159'
+
+pop_ags5_2014 = pop_ags5_2014.groupby(['ags']).sum()
+pop_ags5_2014 = pop_ags5_2014[['pop_t']]
+pop_ags5_2014.rename(columns={'pop_t':'pop_t_2014'}, inplace=True)
+
+
+# merge
+pop_ags5 = pop_ags5_2019.merge(pop_ags5_2014, on='ags')
+
+# generate rate
+pop_ags5['pop_t_growth'] = pop_ags5['pop_t'].astype(int) - pop_ags5['pop_t_2014']
+pop_ags5.drop('pop_t_2014', axis=1, inplace=True)
+
+
 # read out
 pop_ags5.to_csv(z_regional_intermediate + 'population_ags5_prepared.csv', sep=';', encoding='UTF-16', index=False)
-pop_ags8.to_csv(z_regional_intermediate + 'population_ags8_prepared.csv', sep=';', encoding='UTF-16', index=False)
+#pop_ags8.to_csv(z_regional_intermediate + 'population_ags8_prepared.csv', sep=';', encoding='UTF-16', index=False)
 
 
 
--- analysis/prog/prepare/greta_cons_prepare_regional_controls.py
+++ analysis/prog/prepare/greta_cons_prepare_regional_controls.py
@@ -68,7 +68,7 @@ territory['area'] = pd.to_numeric(territory['area'], errors='coerce')
 
 population = pd.read_csv(z_population_intermed + 'population_ags5_prepared.csv',
                          sep=';', encoding='UTF-16', dtype=str)
-population = population[['ags', 'ags_name', 'pop_t']]
+population = population[['ags', 'ags_name', 'pop_t', 'pop_t_growth']]
 population['pop_t'] = pd.to_numeric(population['pop_t'], errors='coerce')
 
 
@@ -78,34 +78,79 @@ population['pop_t'] = pd.to_numeric(population['pop_t'], errors='coerce')
 #           131 workers with degree
 ###############################################################################
 
+
+
+# 2019 ########################################################################
 degree = pd.read_excel(z_regional_source + '131_workers_degree_2019.xlsx',
                           dtype=str, skiprows=15, skipfooter=19)
 degree.columns=['ags', 'ags_name', 'degree', 'total', 'male', 'female', 'f_t', 'f_m', 'f_f']
 degree = degree[['ags', 'ags_name', 'degree', 'total']]
 
-
 # drop irrelevant rows
 degree = degree.loc[degree['total'] != '-'] 
 degree['total'] = pd.to_numeric(degree['total'], errors='coerce')
-
 
 # corect Hamburg & Berlin
 degree.loc[degree['ags']=='02', 'ags'] = '02000'
 degree.loc[degree['ags']=='11', 'ags'] = '11000'
 
-
 # fill in ags & select only ags5
 degree.fillna(method='ffill', inplace=True)
 degree = degree.loc[degree['ags'].str.len() == 5]
-
 
 # pivot table to generate share
 degree = degree.pivot(values='total', index=['ags'],
                     columns='degree')
 
-
-degree['share_university_degree'] = (degree['mit akademischem Abschluss'] / degree['Insgesamt']) *100
+degree['share_uni_degree'] = (degree['mit akademischem Abschluss'] / degree['Insgesamt']) *100
 degree.reset_index(inplace=True)
+degree_2019 = degree.copy()
+
+
+
+
+# 2014 ########################################################################
+degree = pd.read_excel(z_regional_source + '131_workers_degree_2014.xlsx',
+                          dtype=str, skiprows=15, skipfooter=19)
+degree.columns=['ags', 'ags_name', 'degree', 'total', 'male', 'female', 'f_t', 'f_m', 'f_f']
+degree = degree[['ags', 'ags_name', 'degree', 'total']]
+
+# drop irrelevant rows
+degree = degree.loc[degree['total'] != '-'] 
+degree['total'] = pd.to_numeric(degree['total'], errors='coerce')
+
+# corect Hamburg & Berlin
+degree.loc[degree['ags']=='02', 'ags'] = '02000'
+degree.loc[degree['ags']=='11', 'ags'] = '11000'
+
+# fill in ags & select only ags5
+degree.fillna(method='ffill', inplace=True)
+degree = degree.loc[degree['ags'].str.len() == 5]
+
+# pivot table to generate share
+degree = degree.pivot(values='total', index=['ags'],
+                    columns='degree')
+degree.reset_index(inplace=True)
+
+# correct for district reform
+degree.loc[degree['ags']=='03156', 'ags'] = '03159'
+degree.loc[degree['ags']=='03152', 'ags'] = '03159'
+degree = degree.groupby(['ags']).sum() 
+degree.reset_index(inplace=True)
+
+# generate share
+degree['share_uni_degree'] = (degree['mit akademischem Abschluss'] / degree['Insgesamt']) *100
+
+degree = degree[['ags', 'share_uni_degree']]
+degree.rename(columns={'share_uni_degree':'share_uni_degree_2014'}, inplace=True)
+
+
+
+
+## Combine the cross sections #################################################
+degree = degree.merge(degree_2019, on=['ags'])
+degree['share_uni_degree_growth'] = degree['share_uni_degree'] - degree['share_uni_degree_2014']
+degree = degree[['ags', 'share_uni_degree', 'share_uni_degree_growth']]
 
 
 
@@ -113,6 +158,9 @@ degree.reset_index(inplace=True)
 ###############################################################################
 #           132 Unemplyoment
 ###############################################################################
+
+
+# 2019  #######################################################################
 ue = pd.read_excel(z_regional_source + '132_ue_rates_2019.xlsx',
                           dtype=str, skiprows=7, skipfooter=43,
                           usecols= "A, B, J")
@@ -126,6 +174,41 @@ ue.loc[ue['ags']=='11', 'ags'] = '11000'
 ue = ue.loc[ue['ue_rate'] != '-'] 
 ue = ue.loc[ue['ags'].str.len() == 5]
 ue['ue_rate'] = pd.to_numeric(ue['ue_rate'], errors='coerce')
+ue_2019 = ue.copy()
+
+
+
+
+# 2014  #######################################################################
+ue = pd.read_excel(z_regional_source + '132_ue_rates_2014.xlsx',
+                          dtype=str, skiprows=7, skipfooter=43,
+                          usecols= "A, B, J")
+ue.columns =['ags', 'ags_name', 'ue_rate']
+
+# corect Hamburg & Berlin
+ue.loc[ue['ags']=='02', 'ags'] = '02000'
+ue.loc[ue['ags']=='11', 'ags'] = '11000'
+
+# drop irrelevant rows
+ue = ue.loc[ue['ue_rate'] != '-'] 
+ue = ue.loc[ue['ags'].str.len() == 5]
+ue['ue_rate'] = pd.to_numeric(ue['ue_rate'], errors='coerce')
+
+# correct for district reforms
+ue.loc[ue['ags']=='03156', 'ags'] = '03159'
+ue.loc[ue['ags']=='03152', 'ags'] = '03159'
+ue = ue.groupby(['ags']).mean() 
+ue.reset_index(inplace=True)
+ue.rename(columns={'ue_rate':'ue_rate_2014'}, inplace=True)
+
+
+
+
+## Combine the cross sections #################################################
+ue = ue.merge(ue_2019, on='ags')
+ue['ue_rate_growth'] = ue['ue_rate'] - ue['ue_rate_2014']
+ue = ue[['ags', 'ue_rate', 'ue_rate_growth']]
+
 
 
 
@@ -134,22 +217,21 @@ ue['ue_rate'] = pd.to_numeric(ue['ue_rate'], errors='coerce')
 #           133 Sectors
 ###############################################################################
 
+
+# 2019  #######################################################################
 sectors = pd.read_excel(z_regional_source + '133_workers_sector_2018.xlsx',
                           dtype=str, skiprows=7, skipfooter=12)
 sectors.columns = ['ags', 'ags_name', 'total', 'agriculture', 'industry',
                    'processing', 'building', 'trade', 'service', 'public']
 sectors.drop(columns='processing', inplace=True)
 
-
 # corect Hamburg & Berlin
 sectors.loc[sectors['ags']=='02', 'ags'] = '02000'
 sectors.loc[sectors['ags']=='11', 'ags'] = '11000'
 
-
 # drop irrelevant rows
 sectors = sectors.loc[sectors['total'] != '-'] 
 sectors = sectors.loc[sectors['ags'].str.len() == 5]
-
 
 # encode
 z_sector_num_cols = ['total', 'agriculture', 'industry', 
@@ -157,6 +239,35 @@ z_sector_num_cols = ['total', 'agriculture', 'industry',
 for col in z_sector_num_cols:
     sectors[col] = pd.to_numeric(sectors[col], errors='coerce')
 
+# generate shares 
+z_sector_sectors = ['agriculture', 'industry',
+              'building', 'trade', 'service', 'public']
+for col in z_sector_sectors:
+    sectors['share_'+col] = (sectors[col]/sectors['total'])*100
+sectors.drop(columns=['total','ags_name']+z_sector_sectors, inplace=True)
+sectors_2019 = sectors.copy()
+
+
+# 2014  #######################################################################
+sectors = pd.read_excel(z_regional_source + '133_workers_sector_2014.xlsx',
+                          dtype=str, skiprows=7, skipfooter=12)
+sectors.columns = ['ags', 'ags_name', 'total', 'agriculture', 'industry',
+                   'processing', 'building', 'trade', 'service', 'public']
+sectors.drop(columns='processing', inplace=True)
+
+# corect Hamburg & Berlin
+sectors.loc[sectors['ags']=='02', 'ags'] = '02000'
+sectors.loc[sectors['ags']=='11', 'ags'] = '11000'
+
+# drop irrelevant rows
+sectors = sectors.loc[sectors['total'] != '-'] 
+sectors = sectors.loc[sectors['ags'].str.len() == 5]
+
+# encode
+z_sector_num_cols = ['total', 'agriculture', 'industry', 
+              'building', 'trade', 'service', 'public']
+for col in z_sector_num_cols:
+    sectors[col] = pd.to_numeric(sectors[col], errors='coerce')
 
 # generate shares 
 z_sector_sectors = ['agriculture', 'industry',
@@ -165,6 +276,27 @@ for col in z_sector_sectors:
     sectors['share_'+col] = (sectors[col]/sectors['total'])*100
 sectors.drop(columns=['total','ags_name']+z_sector_sectors, inplace=True)
 
+# rename columns
+sectors.rename(columns={'share_agriculture'	:'share_agriculture_2014',
+                        'share_industry'    	:'share_industry_2014',
+                        'share_building'	    :'share_building_2014',
+                        'share_trade'		:'share_trade_2014',
+                        'share_service'		:'share_service_2014',
+                        'share_public'		:'share_public_2014'}, inplace=True)
+
+
+    
+    
+    
+ # combine the cross-sections  ################################################
+sectors = sectors.merge(sectors_2019, on='ags')
+
+for var in ['share_agriculture', 'share_industry', 'share_building', 'share_trade', 'share_service', 'share_public']:
+    sectors[var+'_growth'] = sectors[var] - sectors[var+'_2014']
+    sectors.drop(var+'_2014', axis=1, inplace=True)
+   
+    
+    
 
 
 
@@ -175,8 +307,8 @@ sectors.drop(columns=['total','ags_name']+z_sector_sectors, inplace=True)
 # combine data
 regional = territory[['ags', 'area']].copy()
 regional = regional.merge(population, on='ags')
-regional = regional.merge(degree[['ags', 'share_university_degree']], on='ags')
-regional = regional.merge(ue[['ags', 'ue_rate']], on='ags')
+regional = regional.merge(degree, on='ags')
+regional = regional.merge(ue, on='ags')
 regional = regional.merge(sectors, on='ags')
 
 
